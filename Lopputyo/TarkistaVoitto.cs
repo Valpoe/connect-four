1ad3bbe8626f620aeeece0c0729f9b329d24793b
Mikemu Mikemu <mikko.rautava@outlook.com>
2021-12-04 01:27:16 +0200

    Pohja voitontarkistus.cs classille. Ajatuksen tasolla vain alustettu.

---
+++ Lopputyo/TarkistaVoitto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lopputyo
+{
+    class TarkistaVoitto
+    {
+        public Panel[,] peliKenttä;
+        public int rivi, sarake;
+
+        public void Voitto()
+        {
+            //voitto välittää classin tiedot "Voiton Tarkistus" funktiolle joka katsoo voititko.
+            VoitonTarkistus(peliKenttä, rivi, sarake);
+        }
+
+        private bool VoitonTarkistus(Panel[,] peliKenttä, int tivi, int sarake)
+        {
+            //funktio palauttaa arvon true/false, sillä perusteella voittiko vuorossa ollut pelaaja.
+
+            return true;
+        }
+    }
+}
